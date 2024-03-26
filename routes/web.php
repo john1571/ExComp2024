7a3a6b8b06b5b2d9f329d825ea652b00b0258582
John1571 <johnny96jones@gmail.com>
2024-03-25 22:48:01 -0500

    #2 Create workouts branch

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ Route::get('/user', function() {
     return view('user', ['users' => User::all()]);
 });
 
-Route::get('/workout', [WorkoutController::class, 'show']);
-Route::get('/new_workout', [WorkoutController::class, 'new']);
+Route::get('workout', [WorkoutController::class, 'show']);
+Route::get('new_workout', [WorkoutController::class, 'new']);
 Route::post('workout', [WorkoutController::class, 'create']);
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
